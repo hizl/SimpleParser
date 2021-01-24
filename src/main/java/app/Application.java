7c0e0bc5e2d8a5f54c50a1dec9555da3d8fbf776
dev <hizlle1817@gmail.com>
2021-01-25 01:01:17 +0500

    adding exception handlers, updating the readme

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,15 +1,15 @@
 package app;
 
 import io.Reader;
+import util.Util;
 
 import java.io.IOException;
 
 class Application {
     public static void main(String[] args) throws IOException {
-
-        Reader Reader = new Reader();
-        Reader.run();
-
-
+        Reader reader = new Reader();
+        Util util = new Util();
+        reader.run();
+        util.displayingRows();
     }
 }
--- src/main/java/io/Writer.java
+++ src/main/java/io/Writer.java
@@ -1,50 +1,44 @@
 package io;
 
-import util.Worker;
-
 import java.io.*;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
-
-/*import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;*/
-
 public class Writer {
-
-    public static void processingFiles(String str) throws FileNotFoundException, IOException {
-
-        Path path = Paths.get("/home/dev/IdeaProjects/SimpleParser/src/main/java/resources/file.txt");
-        File file = Path.of("/home/dev/IdeaProjects/SimpleParser/src/main/java/resources/", "file.txt").toFile();
-
-        FileReader fileReader = new FileReader(file);
-
+    public static void createNewFileAndRecordResult(String str) {
+        File file = new File("/home/dev/IdeaProjects/SimpleParser/src/main/java/resources/parse.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Incorrect create file path " + e.getMessage());
+        }
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("file is not exist " + e.getMessage());
+        }
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-
         String result = "";
         while (str != null) {
             result += str + System.lineSeparator();
-            str = bufferedReader.readLine();
+            try {
+                str = bufferedReader.readLine();
+            } catch (IOException e) {
+                System.out.println("file is not exist " + e.getMessage());
+            }
+        }
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+            fileWriter.write(result);
+            fileWriter.close();
+            fileReader.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("file is not exist " + e.getMessage());
+            e.printStackTrace();
         }
 
-       // Worker.displayingRows(path);
-        Path resultPath = Path.of("/home/dev/IdeaProjects/SimpleParser/src/main/java/resources/", "result.txt");
-
-
-
-
-
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write(result);
-        fileWriter.close();
-        fileReader.close();
-        bufferedReader.close();
     }
-
 }
 
 
